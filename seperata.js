34d611fadb7d041ff058f4d7011685ef626f7fde
javierfurus <javierfurus@gmail.com>
2020-05-15 19:00:38 +0200

    added separate.js

---
+++ seperata.js
@@ -0,0 +1,35 @@
+// Here are two methods to separate items in arrays
+const separate = (src) => {
+  const evens = [];
+  const odds = [];
+  let evenIndex = 0;
+  let oddIndex = 0;
+  for (let i = 0; i < src.length; i++) {
+    if (src[i] % 2 === 0) {
+      evens[evenIndex++] = src[i];
+    } else {
+      odds[oddIndex++] = src[i];
+    }
+  }
+  return {
+    evens,
+    odds
+  };
+};
+
+const separateByReference = (src, evens, odds) => {
+  let evenIndex = 0;
+  let oddIndex = 0;
+  for (let i = 0; i < src.length; i++) {
+    if (src[i] % 2 === 0) {
+      evens[evenIndex++] = src[i];
+    } else {
+      odds[oddIndex++] = src[i];
+    }
+  }
+};
+
+const evens = [];
+const odds = [];
+separateByReference([1, 2, 3, 4, 5], evens, odds);
+console.log('evens, odds', evens, odds);
