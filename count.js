030eba030669f1903b8e4ee0607ffe6b1de180cf
javierfurus <javierfurus@gmail.com>
2020-05-15 18:58:00 +0200

    added count.js

---
+++ count.js
@@ -0,0 +1,11 @@
+// Function to count those elements in the array
+const countElement = (arr, element) => {
+  let count = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === element) {
+      count++;
+    }
+  }
+  return count;
+};
+console.log('Count 1:', countElement([1, 2, 1, 3, 1], 1));
