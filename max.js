766178485a953c79fc7f4112373cccb5676c5689
javierfurus <javierfurus@gmail.com>
2020-05-15 18:58:42 +0200

    added max.js

---
+++ max.js
@@ -0,0 +1,12 @@
+// Find the biggest item in an array
+const maxArr = (arr) => {
+  let max = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+  return max;
+};
+
+console.log('Max', maxArr([300, -5, 2, 10, 20, -2]));
