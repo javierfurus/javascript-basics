3e2797b5ad639ea97c7190254268d8778a409655
javierfurus <javierfurus@gmail.com>
2020-05-15 18:59:17 +0200

    added search.js

---
+++ search.js
@@ -0,0 +1,11 @@
+// This function will help you see where an item is located in an array
+const indexOfArray = (arr, element) => {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === element) {
+      return i;
+    }
+  }
+  return -1;
+};
+
+console.log('IndexOf', indexOfArray([1, 2, 3, 4], 5));
